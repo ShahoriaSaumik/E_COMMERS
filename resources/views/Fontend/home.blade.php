427dec8416b2c24f6a3937319e8ab5f502be8d74
ShahoriaSaumik <shahoriasaumik.com>
2025-04-22 20:08:02 +0600

    bakend and fontent user dashboard layout create

---
+++ resources/views/Fontend/home.blade.php
@@ -0,0 +1,18 @@
+@extends('Fontend.layouts.master')
+
+@section('title', 'Home')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <h1>This is home page</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
+use App\Http\Controllers\Bakend\user\DashboardController as UserDashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' =>  'auth', 'prefix' => 'user', 'as' => 'user.'], function () {
+    Route::get('/dashboard', [UserDashboardController::class, 'dashboard'])->name('dashboard');
 });
